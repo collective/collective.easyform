8144598f886a812f5d12186dbbe4c03f13a9b3db
Roman Kozlovskyi <krzroman@gmail.com>
2014-01-06 14:07:16 +0200

    testEvents

--- collective/formulator/tests/testEvents.py
+++ collective/formulator/tests/testEvents.py
@@ -2,13 +2,14 @@
 # Test PloneFormGen event-handler functionality
 #
 
-import os
-import sys
+#import os
+#import sys
 
 from collective.formulator.tests import pfgtc
+from collective.formulator.api import get_actions, get_fields
 
-if __name__ == '__main__':
-    execfile(os.path.join(sys.path[0], 'framework.py'))
+#if __name__ == '__main__':
+    #execfile(os.path.join(sys.path[0], 'framework.py'))
 
 
 class TestAdapterPaste(pfgtc.PloneFormGenTestCase):
@@ -26,41 +27,19 @@ class TestAdapterPaste(pfgtc.PloneFormGenTestCase):
         self.folder.invokeFactory('Formulator', 'ff1')
         self.ff1 = getattr(self.folder, 'ff1')
 
-    def testPastedAdaptersAreActive(self):
-        self.loginAsPortalOwner()
-                                # creating a FormCustomScriptAdapter requires
-                                # elevated permissions
-        for f in self.adapterTypes:
-            fname = "%s1" % f
-            self.ff1.invokeFactory(f, fname)
-            #f1 = getattr(self.ff1, fname)
-            copy = self.ff1.manage_copyObjects(fname)
-            new_id = self.ff1.manage_pasteObjects(copy)[0]['new_id']
-
-            self.assertTrue(new_id in self.ff1.getActionAdapter())
-
     def testActiveAdaptersNotDuplicatedOnFormCopy(self):
         self.loginAsPortalOwner()
         copy = self.folder.manage_copyObjects('ff1')
         new_id = self.folder.manage_pasteObjects(copy)[0]['new_id']
         ff2 = getattr(self.folder, new_id)
-        active_adapters = ff2.getActionAdapter()
+        active_adapters = tuple(get_actions(ff2))
         self.assertEqual(active_adapters, ('mailer',))
-
-    def testTempFactoryAdaptersNotActivatedOnForm(self):
-        self.loginAsPortalOwner()
-                                # creating a FormCustomScriptAdapter requires
-                                # elevated permissions
-        adapter_count = len(self.ff1.getActionAdapter())
-        for f in self.adapterTypes:
-            self.ff1.restrictedTraverse(
-                'portal_factory/%s/%s_tmp' % (f, f))
-        # temporary objects shouldn't be active on the form folder
-        self.assertEqual(adapter_count, len(self.ff1.getActionAdapter()))
+        active_fields = tuple([i for i in get_fields(ff2)])
+        self.assertEqual(active_fields, ('replyto', 'topic', 'comments'))
 
 
 def test_suite():
     from unittest import TestSuite, makeSuite
     suite = TestSuite()
-    #suite.addTest(makeSuite(TestAdapterPaste))
+    suite.addTest(makeSuite(TestAdapterPaste))
     return suite
