d6bce4c3cd36efe0d089534086f5b1ce8ff42553
prakhar joshi <prakhar126@gmail.com>
2016-07-19 20:59:17 +0530

    Update uninstall profile for easyform
    add custome page for easyform config
    
    Update registry prefix name
    
    Fix to access control panel on given url

--- collective/easyform/setuphandlers.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-logger = logging.getLogger('collective.easyform.setuphandlers')
-
-
-def uninstall(context):
-    setup_tool = context.portal_setup
-    setup_tool.runAllImportStepsFromProfile('profile-collective.easyform:uninstall')
