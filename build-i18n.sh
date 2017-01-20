38aec3d8adf3051ffbace35c1f4469f052c3b359
Tom Gross <itconsense@gmail.com>
2017-01-20 09:49:38 +0000

    updated *.po files and some german translations

--- build-i18n.sh
+++ build-i18n.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-I18NDUDE=i18ndude
-I18NPATH=collective/easyform
+I18NDUDE=bin/i18ndude
+I18NPATH=src/collective/easyform
 DOMAIN=collective.easyform
 $I18NDUDE rebuild-pot --pot $I18NPATH/locales/$DOMAIN.pot --create $DOMAIN $I18NPATH
 $I18NDUDE sync --pot $I18NPATH/locales/$DOMAIN.pot $I18NPATH/locales/*/LC_MESSAGES/$DOMAIN.po
