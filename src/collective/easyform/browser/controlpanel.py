5a70ebf037f5218608038a460a64a5ae9b829bab
Tom Gross <itconsense@gmail.com>
2017-03-08 07:09:50 +0000

    Allow managment of fields enabled for c.easyform in controlpanel

--- src/collective/easyform/browser/controlpanel.py
+++ src/collective/easyform/browser/controlpanel.py
@@ -1,9 +1,44 @@
 # -*- coding: utf-8 -*-
+from collective.easyform import easyformMessageFactory as _
 from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
 from plone.app.registry.browser.controlpanel import RegistryEditForm
+from plone.registry.interfaces import IRegistry
+from plone.schemaeditor.interfaces import IFieldFactory
 from plone.z3cform import layout
+from z3c.form.browser.checkbox import CheckBoxFieldWidget
 from zope import schema
+from zope.component import getUtilitiesFor
+from zope.component import getUtility
+from zope.globalrequest import getRequest
+from zope.i18n import translate
+from zope.interface import implementer
 from zope.interface import Interface
+from zope.schema.interfaces import IVocabularyFactory
+from zope.schema.vocabulary import SimpleVocabulary
+
+import operator
+
+
+def getContent(self):
+        return getUtility(IRegistry).forInterface(
+            self.schema,
+            prefix=self.schema_prefix)
+
+
+@implementer(IVocabularyFactory)
+def FieldsVocabularyFactory(context=None):
+    request = getRequest()
+    terms = []
+    for (field_id, factory) in getUtilitiesFor(IFieldFactory):
+        terms.append(
+            SimpleVocabulary.createTerm(
+                field_id,
+                factory.title,
+                translate(factory.title, context=request)
+            )
+        )
+    terms = sorted(terms, key=operator.attrgetter('title'))
+    return SimpleVocabulary(terms)
 
 
 class IEasyFormControlPanel(Interface):
@@ -16,11 +51,30 @@ class IEasyFormControlPanel(Interface):
         default=False,
     )
 
+    allowedFields = schema.List(
+        title=_(u"Allowed Fields"),
+        description=_(u"This Fields are available for your forms."),
+        value_type=schema.Choice(
+            description=_(
+                u"help_registry_items",
+                default=u"Select the registry items you desire to modify"
+            ),
+            required=False,
+            vocabulary='collective.easyform.FieldsVocabulary',
+        ),
+        default=[],
+    )
+
 
 class EasyFormControlPanelForm(RegistryEditForm):
     schema = IEasyFormControlPanel
-    schema_prefix = "easyform"
+    schema_prefix = 'easyform'
     label = u'easyform Settings'
+
+    def updateFields(self):
+        super(EasyFormControlPanelForm, self).updateFields()
+        self.fields['allowedFields'].widgetFactory = \
+            CheckBoxFieldWidget
 
 
 EasyFormControlPanelView = layout.wrap_form(
---
+++ src/collective/easyform/tests/testControlpanel.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+from collective.easyform.tests import base
+from plone.app.testing import SITE_OWNER_NAME
+from plone.app.testing import SITE_OWNER_PASSWORD
+from plone.registry.interfaces import IRegistry
+from plone.testing.z2 import Browser
+from zope.component import getUtility
+
+
+class AllowedFieldsControlPanelFuncionalTest(base.EasyFormTestCase):
+    """Test that changes in the easyform control panel are actually
+    stored in the registry.
+    """
+
+    def setUpPloneSite(self):
+        pass
+
+    def setUp(self):
+        self.app = self.layer['app']
+        self.portal = self.layer['portal']
+        self.portal_url = self.portal.absolute_url()
+        self.browser = Browser(self.app)
+        self.browser.handleErrors = False
+        self.browser.addHeader(
+            'Authorization',
+            'Basic ' + SITE_OWNER_NAME + ':' + SITE_OWNER_PASSWORD
+        )
+
+    def test_easyform_control_panel_link(self):
+        self.browser.open(
+            self.portal_url + '/@@overview-controlpanel')
+        self.browser.getLink('easyform').click()
+        self.assertTrue('easyform Settings' in self.browser.contents)
+
+    def test_easyform_control_panel_backlink(self):
+        self.browser.open(
+            self.portal_url + '/@@easyform-controlpanel')
+        self.assertTrue('General' in self.browser.contents)
+
+    def test_easyform_control_panel_sidebar(self):
+        self.browser.open(
+            self.portal_url + '/@@navigation-controlpanel')
+        self.browser.getLink('Site Setup').click()
+        self.assertEqual(
+            self.browser.url,
+            'http://nohost/plone/@@overview-controlpanel')
+
+    def test_easyform_control_panel_checkbox(self):
+        self.browser.open(
+            self.portal_url + '/@@easyform-controlpanel')
+        self.browser.getControl('Rich Text').selected = False
+        self.browser.getControl('Save').click()
+        registry = getUtility(IRegistry)
+        self.assertNotIn(
+            'plone.app.textfield.RichText',
+            registry.records['easyform.allowedFields'].value)
+
+# EOF
