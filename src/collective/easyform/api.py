dd911a3c4bea4a2e5a4bb081b4b64c6932fcdec2
Tom Gross <itconsense@gmail.com>
2017-03-18 07:34:01 +0000

    Bring back old dollar replacer

--- src/collective/easyform/api.py
+++ src/collective/easyform/api.py
@@ -13,6 +13,55 @@ from types import StringTypes
 
 
 CONTEXT_KEY = u'context'
+# regular expression for dollar-sign variable replacement.
+# we want to find ${identifier} patterns
+dollarRE = compile(r'\$\{(.+?)\}')
+
+
+class DollarVarReplacer(object):
+
+    """
+    Initialize with a dictionary, then self.sub returns a string
+    with all ${key} substrings replaced with values looked
+    up from the dictionary.
+
+    >>> from collective.easyform import api
+
+    >>> adict = {'one':'two', '_two':'three', '.two':'four'}
+    >>> dvr = api.DollarVarReplacer(adict)
+
+    >>> dvr.sub('one one')
+    'one one'
+
+    >>> dvr.sub('one ${one}')
+    'one two'
+
+    >>> dvr.sub('one ${two}')
+    'one ???'
+
+    Skip any key beginning with _ or .
+    >>> dvr.sub('one ${_two}')
+    'one ???'
+
+    >>> dvr.sub('one ${.two}')
+    'one ???'
+
+    """
+
+    def __init__(self, adict):
+        self.adict = adict
+
+    def sub(self, s):
+        return dollarRE.sub(self.repl, s)
+
+    def repl(self, mo):
+        key = mo.group(1)
+        if key and key[0] not in ['_', '.']:
+            try:
+                return self.adict[mo.group(1)]
+            except KeyError:
+                pass
+        return '???'
 
 
 def get_expression(context, expression_string, **kwargs):
@@ -179,4 +228,5 @@ def format_addresses(addresses, names=[]):
 
 
 def dollar_replacer(s, data):
-    return s.replace('${', '{').format(**data)
+    dr = DollarVarReplacer(data)
+    return dr.sub(s)
