c99a887bcf724a07d63aa59c201917e4f5627441
Dylan Jay <software@pretaweb.com>
2019-04-01 10:16:35 +0700

    add back in 5.0 plone classifier

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
         'Environment :: Web Environment',
         'Framework :: Plone',
         'Framework :: Plone :: Addon',
+        'Framework :: Plone :: 5.0',
         'Framework :: Plone :: 5.1',
         'Framework :: Plone :: 5.2',
         'Programming Language :: Python',
