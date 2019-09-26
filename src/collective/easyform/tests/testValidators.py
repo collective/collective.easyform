292f859f4a8528bb4537ef9be7e0a3b496524d86
Dylan Jay <software@pretaweb.com>
2019-09-26 21:34:18 +0700

    simply test

--- src/collective/easyform/tests/testValidators.py
+++ src/collective/easyform/tests/testValidators.py
@@ -104,7 +104,8 @@ class TestBaseValidators(base.EasyFormTestCase):
         self.assertEqual(len(errors), 1)
 
 
-class TestSingleFieldValidator(base.EasyFormTestCase):
+
+class LoadFixtureBase(base.EasyFormTestCase):
 
     """ test validator in form outside of fieldset
 
@@ -134,6 +135,16 @@ class TestSingleFieldValidator(base.EasyFormTestCase):
         for key in kwargs.keys():
             request.form[prefix + key] = kwargs[key]
         return request
+
+
+class TestSingleFieldValidator(LoadFixtureBase):
+
+    """ test validator in form outside of fieldset
+
+    The test methods are reused in TestFieldsetValidator.
+    They use the same field, except that one has it in a fieldset.
+    """
+    schema_fixture = "single_field.xml"
 
     def test_get_default(self):
         # With a GET, we should see the default value in the form.
@@ -342,25 +353,14 @@ class TestSizeValidator(base.EasyFormTestCase):
         self.assertEqual(translate(validation), u'File type "" is not allowed!')
 
 
-class TestSingleRecaptchaValidator(base.EasyFormTestCase):
+class TestSingleRecaptchaValidator(LoadFixtureBase):
 
     """ Can't test captcha passes but we can test it fails
     """
     schema_fixture = "recaptcha.xml"
 
     def afterSetUp(self):
-        self.folder.invokeFactory("EasyForm", "ff1")
-        self.ff1 = getattr(self.folder, "ff1")
-        self.ff1.CSRFProtection = False  # no csrf protection
-        self.ff1.showAll = True
-        field_template = api.content.create(
-            self.layer["portal"], "File", id="easyform_default_fields.xml"
-        )
-        with open(join(dirname(__file__), "fixtures", self.schema_fixture)) as f:
-            filecontent = NamedFile(f.read(), contentType="application/xml")
-        field_template.file = filecontent
-        classImplements(BaseRequest, IFormLayer)
-        validators.update_validators()
+        super(TestSingleRecaptchaValidator, self).afterSetUp()
 
         # Put some dummy values for recaptcha
         registry = getUtility(IRegistry)
@@ -368,13 +368,6 @@ class TestSingleRecaptchaValidator(base.EasyFormTestCase):
         proxy.public_key = u"foo"
         proxy.private_key = u"bar"
 
-    def LoadRequestForm(self, **kwargs):
-        request = self.layer["request"]
-        request.form.clear()
-        prefix = "form.widgets."
-        for key in kwargs.keys():
-            request.form[prefix + key] = kwargs[key]
-        return request
 
     def test_no_answer(self):
         data = {"verification": ""}
