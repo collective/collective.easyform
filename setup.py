905fe70858c3d73b8b6dfcf088abf9f8938d1b53
Jens W. Klein <jk@kleinundpartner.at>
2020-09-25 18:51:56 +0200

    Works with Python 3.8

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
