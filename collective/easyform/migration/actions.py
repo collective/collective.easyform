c332ff5c70238bd1ffb0dc8f0c43620ac7759872
Thibaut Born <thibaut.born@kuleuven.be>
2022-02-08 17:32:05 +0100

    black

--- collective/easyform/migration/actions.py
+++ collective/easyform/migration/actions.py
@@ -11,19 +11,20 @@ from Products.PloneFormGen.content.actionAdapter import FormActionAdapter
 import logging
 
 try:
-  from collective.easyform.interfaces.mailer import default_mail_body
+    from collective.easyform.interfaces.mailer import default_mail_body
 except ImportError:
-  from collective.easyform.config import MAIL_BODY_DEFAULT
-  def default_mail_body():
-    return MAIL_BODY_DEFAULT
+    from collective.easyform.config import MAIL_BODY_DEFAULT
+
+    def default_mail_body():
+        return MAIL_BODY_DEFAULT
 
 
-logger = logging.getLogger('collective.easyform.migration')
+logger = logging.getLogger("collective.easyform.migration")
 
 
 NAMESPACES = {
-    'easyform': 'http://namespaces.plone.org/supermodel/easyform',
-    'form': 'http://namespaces.plone.org/supermodel/form',
+    "easyform": "http://namespaces.plone.org/supermodel/easyform",
+    "form": "http://namespaces.plone.org/supermodel/form",
 }
 
 
@@ -34,48 +35,50 @@ def append_body_pt(field, name, value):
     node.text = default_mail_body()
 
 
-Type = namedtuple('Type', ['name', 'handler'])
-Property = namedtuple('Property', ['name', 'handler'])
+Type = namedtuple("Type", ["name", "handler"])
+Property = namedtuple("Property", ["name", "handler"])
 
 
 TYPES_MAPPING = {
-    'FormMailerAdapter': Type('collective.easyform.actions.Mailer', append_field),
-    'FormSaveDataAdapter': Type('collective.easyform.actions.SaveData', append_field),
-    'FormCustomScriptAdapter': Type('collective.easyform.actions.CustomScript', append_field),
+    "FormMailerAdapter": Type("collective.easyform.actions.Mailer", append_field),
+    "FormSaveDataAdapter": Type("collective.easyform.actions.SaveData", append_field),
+    "FormCustomScriptAdapter": Type(
+        "collective.easyform.actions.CustomScript", append_field
+    ),
 }
 
 PROPERTIES_MAPPING = {
-    'additional_headers': Property('additional_headers', append_node),
-    'bcc_recipients': Property('bcc_recipients', append_node),
-    'bccOverride': Property('bccOverride', append_node),
-    'body_footer': Property('body_footer', append_node),
-    'body_post': Property('body_post', append_node),
-    'body_pre': Property('body_pre', append_node),
-    'body_pt': Property('body_pt', append_body_pt),
-    'body_type': Property('body_type', append_node),
-    'cc_recipients': Property('cc_recipients', append_node),
-    'ccOverride': Property('ccOverride', append_node),
-    'description': Property('description', append_node),
-    'execCondition': Property('easyform:execCondition', set_attribute),
-    'msg_subject': Property('msg_subject', append_node),
-    'recipient_email': Property('recipient_email', append_node),
-    'recipient_name': Property('recipient_name', append_node),
-    'recipientOverride': Property('recipientOverride', append_node),
-    'replyto_field': Property('replyto_field', append_node),
-    'senderOverride': Property('senderOverride', append_node),
-    'showAll': Property('showAll', append_node),
-    'showFields': Property('showFields', append_list_node),
-    'subject_field': Property('subject_field', append_node),
-    'subjectOverride': Property('subjectOverride', append_node),
-    'title': Property('title', append_node),
-    'to_field': Property('to_field', append_node),
-    'xinfo_headers': Property('xinfo_headers', append_node),
-    'ExtraData': Property('ExtraData', append_list_node),
-    'DownloadFormat': Property('DownloadFormat', append_node),
-    'UseColumnNames': Property('UseColumnNames', append_node),
-    'SavedFormInput': Property('SavedFormInput', append_node),
-    'ProxyRole': Property('ProxyRole', append_node),
-    'ScriptBody': Property('ScriptBody', append_node),
+    "additional_headers": Property("additional_headers", append_node),
+    "bcc_recipients": Property("bcc_recipients", append_node),
+    "bccOverride": Property("bccOverride", append_node),
+    "body_footer": Property("body_footer", append_node),
+    "body_post": Property("body_post", append_node),
+    "body_pre": Property("body_pre", append_node),
+    "body_pt": Property("body_pt", append_body_pt),
+    "body_type": Property("body_type", append_node),
+    "cc_recipients": Property("cc_recipients", append_node),
+    "ccOverride": Property("ccOverride", append_node),
+    "description": Property("description", append_node),
+    "execCondition": Property("easyform:execCondition", set_attribute),
+    "msg_subject": Property("msg_subject", append_node),
+    "recipient_email": Property("recipient_email", append_node),
+    "recipient_name": Property("recipient_name", append_node),
+    "recipientOverride": Property("recipientOverride", append_node),
+    "replyto_field": Property("replyto_field", append_node),
+    "senderOverride": Property("senderOverride", append_node),
+    "showAll": Property("showAll", append_node),
+    "showFields": Property("showFields", append_list_node),
+    "subject_field": Property("subject_field", append_node),
+    "subjectOverride": Property("subjectOverride", append_node),
+    "title": Property("title", append_node),
+    "to_field": Property("to_field", append_node),
+    "xinfo_headers": Property("xinfo_headers", append_node),
+    "ExtraData": Property("ExtraData", append_list_node),
+    "DownloadFormat": Property("DownloadFormat", append_node),
+    "UseColumnNames": Property("UseColumnNames", append_node),
+    "SavedFormInput": Property("SavedFormInput", append_node),
+    "ProxyRole": Property("ProxyRole", append_node),
+    "ScriptBody": Property("ScriptBody", append_node),
 }
 
 
@@ -86,7 +89,7 @@ def pfg_actions(context):
             continue
         id_ = obj.getId()
         props = {}
-        props['_portal_type'] = obj.portal_type
+        props["_portal_type"] = obj.portal_type
         for field in obj.Schema().fields():
             name = field.getName()
             accessor = field.getEditAccessor(obj)
@@ -99,18 +102,20 @@ def actions_model(ploneformgen):
     """Create an actions xml model from a PloneFormGen instance."""
     parser = etree.XMLParser(remove_blank_text=True)
     model = etree.fromstring(ACTIONS_MODEL, parser)
-    schema = model.find(
-        '{http://namespaces.plone.org/supermodel/schema}schema')
+    schema = model.find("{http://namespaces.plone.org/supermodel/schema}schema")
 
     for actionname, properties in pfg_actions(ploneformgen):
-        type_ = TYPES_MAPPING.get(properties['_portal_type'])
+        type_ = TYPES_MAPPING.get(properties["_portal_type"])
         if type_ is None:
             # ploneformgen has been renamed with _MIGRATION_ suffix
             # hence, we remove 11 chars
             pfgpath = "/".join(ploneformgen.getPhysicalPath())[:-11]
             logger.warning(
                 "Ignoring field '%s' of type '%s' of form '%s.",
-                actionname, properties['_portal_type'], pfgpath)
+                actionname,
+                properties["_portal_type"],
+                pfgpath,
+            )
             continue
 
         field = type_.handler(schema, type_.name, actionname, properties)
