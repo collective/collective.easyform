4b529bd2b778f892d31d9203e99736ba8a609274
Thibaut Born <thibaut.born@kuleuven.be>
2022-03-22 15:48:28 +0100

    convert data not serializable by default json
    context: Some data (date, set..) could not be serialized to json

--- collective/easyform/migration/serializer.py
+++ collective/easyform/migration/serializer.py
@@ -1,20 +1,30 @@
-from plone.restapi.interfaces import ISerializeToJson
-from plone.restapi.interfaces import IDeserializeFromJson
+import datetime
+from datetime import date, datetime
+from dateutil import parser
+import json
+import logging
+
 from zope.component import adapter
 from zope.interface import implementer
 from zope.interface import Interface
-from collective.easyform.interfaces import IEasyForm
+from zope.schema import getFieldsInOrder
+from zope.schema.interfaces import ISet, IDate, IDatetime
+
 from plone.restapi.serializer.dxcontent import SerializeToJson as DXContentToJson
 from plone.restapi.deserializer.dxcontent import (
     DeserializeFromJson as DXContentFromJson,
 )
 from plone.restapi.deserializer import json_body
-from zope.schema import getFieldsInOrder
-from collective.easyform.api import get_actions
-from collective.easyform.interfaces import ISaveData
-import json
+from plone.restapi.interfaces import ISerializeToJson
+from plone.restapi.interfaces import IDeserializeFromJson
+from plone.app.textfield.value import RichTextValue
+from plone.app.textfield.interfaces import IRichText
+from plone.app.textfield.value import RichTextValue
 
-import logging
+from collective.easyform.api import get_actions
+from collective.easyform.api import get_fields
+from collective.easyform.interfaces import IEasyForm
+from collective.easyform.interfaces import ISaveData
 
 
 logger = logging.getLogger("collective.easyform.migration")
@@ -32,23 +42,46 @@ class SerializeToJson(DXContentToJson):
     def serializeSavedData(self, result):
         storage = dict()
         actions = getFieldsInOrder(get_actions(self.context))
+        field_names = get_fields(self.context).names()
+        field_names.sort()
         for name, action in actions:
             if ISaveData.providedBy(action):
                 serializeable = dict()
                 storage[name] = serializeable
-                for key, value in action.getSavedFormInputItems():
+                for id, data in action.getSavedFormInputItems():
+                    column_names = data.keys()
+                    column_names.remove("id")
+                    column_names.sort()
+                    if column_names != field_names:
+                        logger.warning(
+                            "Skipped Saveddata row because of field_names mismatch in %s",
+                            self.context.absolute_url(),
+                        )
+                        continue
                     try:
-                        json.dumps(value)
-                        serializeable[key] = value
+                        for key, value in data.items():
+                            data[key] = convertBeforeSerialize(value)
+                        json.dumps(data)
+                        serializeable[id] = data
                     except TypeError:
                         logger.exception(
                             "saved data serialization issue for {}".format(
                                 self.context.absolute_url()
                             )
                         )
-
         if storage:
             result["savedDataStorage"] = storage
+
+
+def convertBeforeSerialize(value):
+    if isinstance(value, (datetime, date)):
+        return value.isoformat()
+    elif isinstance(value, set):
+        return list(value)
+    elif isinstance(value, RichTextValue):
+        return value.raw_encoded
+    else:
+        return value
 
 
 @implementer(IDeserializeFromJson)
@@ -72,8 +105,25 @@ class DeserializeFromJson(DXContentFromJson):
         if data.has_key("savedDataStorage"):
             storage = data["savedDataStorage"]
             actions = getFieldsInOrder(get_actions(self.context))
+            schema = get_fields(self.context)
+
             for name, action in actions:
                 if ISaveData.providedBy(action) and storage.has_key(name):
                     savedData = storage[name]
                     for key, value in savedData.items():
+                        for name in schema.names():
+                            value[name] = convertAfterDeserialize(
+                                schema[name], value[name]
+                            )
                         action.setDataRow(int(key), value)
+
+
+def convertAfterDeserialize(field, value):
+    if ISet.providedBy(field):
+        return set(value)
+    elif IDate.providedBy(field) or IDatetime.providedBy(field):
+        return parser.parse(value)
+    elif IRichText.providedBy(field):
+        return RichTextValue(value)
+    else:
+        return value
