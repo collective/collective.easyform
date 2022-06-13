b332d42580b5ab6a28591b0f668372404428a616
Laurent Lasudry <lasudry@gmail.com>
2022-06-13 20:28:16 +0200

    Add translation for default mail subject

--- src/collective/easyform/interfaces/mailer.py
+++ src/collective/easyform/interfaces/mailer.py
@@ -13,7 +13,10 @@ from Products.CMFPlone.utils import safe_unicode
 from z3c.form.browser.checkbox import CheckBoxFieldWidget
 from z3c.form.browser.textarea import TextAreaWidget
 from plone.autoform.interfaces import OMITTED_KEY
-
+from zope.globalrequest import getRequest
+from zope.i18n import translate
+from zope.interface import provider
+from zope.schema.interfaces import IContextAwareDefaultFactory
 
 import zope.i18nmessageid
 import zope.interface
@@ -22,6 +25,11 @@ import zope.schema.interfaces
 
 PMF = zope.i18nmessageid.MessageFactory("plone")
 MODIFY_PORTAL_CONTENT = "cmf.ModifyPortalContent"
+
+
+@provider(IContextAwareDefaultFactory)
+def default_mail_subject(context):
+    return translate(_(u"Form Submission"), context=getRequest())
 
 
 def default_mail_body():
@@ -161,7 +169,7 @@ class IMailer(IAction):
             u"do not specify a subject field or if the field "
             u"is empty.",
         ),
-        default=u"Form Submission",
+        defaultFactory=default_mail_subject,
         missing_value=u"",
         required=False,
     )
