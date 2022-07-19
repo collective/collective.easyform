031df381be7b219749be705fd45ea4b8f7342449
Your Name <m.leimgruber@4teamwork.ch>
2022-07-19 11:14:24 -0400

    Use "|" as delimiter of list types in exports.
    This makes the for example multiple choice fields in Excel/Spreadsheets
    way more controllable.
    
    Signed-off-by: Your Name <m.leimgruber@4teamwork.ch>

--- src/collective/easyform/actions.py
+++ src/collective/easyform/actions.py
@@ -697,6 +697,10 @@ class SaveData(Action):
                 data = data.filename
             if six.PY2 and isinstance(data, six.text_type):
                 return data.encode("utf-8")
+            if isinstance(data, (list, tuple, set)):
+                data = '|'.join(data)
+                if six.PY2:
+                    return data.encode('utf-8')
             return data
 
         return [get_data(row, i) for i in names]
