c8676032eddce8ddb56905f3eb2d9c7b3ab15e6e
Thibaut Born <thibaut.born@kuleuven.be>
2022-09-30 19:26:18 +0200

    refactor saved_data migration
    context: to better customize specific parts & make code more modular

--- collective/easyform/migration/data.py
+++ collective/easyform/migration/data.py
@@ -17,91 +17,100 @@ import logging
 
 logger = logging.getLogger("collective.easyform.migration")
 
-def should_migrate_row(data_adapter, easyform, idx, row, has_failed_row):
+def should_migrate_row(data_adapter, easyform, row, idx):
     cols = data_adapter.getColumnNames()
-    if len(row) != len(cols):
-        if not has_failed_row:
-            logger.warning(
-                "Number of columns does not match for all rows. Some data were skipped in "
-                "data adapter %s/%s",
-                "/".join(easyform.getPhysicalPath()),
-                data_adapter.getId(),
-            )
+    if len(row) == len(cols):
+        return True
+    else:
         logger.info(
             "Column count mismatch at row %s",
             idx,
         )
         return False
-    else:
+
+def is_valid_value(key, value, easyform):
+    schema = get_fields(easyform)
+    try:
+        field = schema.get(key)
+        value = value.decode("utf8")
+        if IFromUnicode.providedBy(field) and value:
+            value = field.fromUnicode(value)
+        elif IDatetime.providedBy(field) and value:
+            value = DateTime.DateTime(value).asdatetime()
+        elif IDate.providedBy(field) and value:
+            value = DateTime.DateTime(value).asdatetime().date()
+        elif ISet.providedBy(field):
+            value = set(literal_eval(value))
+        elif INamedBlobFileField.providedBy(field):
+            value = None
         return True
+    except (
+        ValueError,
+        TypeError,
+        ValidationError,
+        SyntaxError,
+        DateTime.interfaces.SyntaxError,
+        DateTime.interfaces.TimeError,
+        DateTime.interfaces.DateError
+    ) as error:
+        # Exceptions above are often due to long living Forms, where users have changed their minds about
+        # the Field formats/widgets...
+        # Older datarows can break in these cases
+
+        try:
+            value_to_log = safe_unicode(value)
+        except UnicodeError:
+            # in case of file field, try to log the file name
+            # which is seperated with a colon
+            if ':' in value:
+                value_to_log = safe_unicode(value.split(':')[0])
+            else:
+                # xxx we hope we are lucky that there are no broken characters in the first 50
+                value_to_log = safe_unicode(value)[:50]
+
+        logger.exception(
+            u"Error for {}:'{}' in the {} data adapter.".format(
+                key,
+                value_to_log,
+                "/".join(easyform.getPhysicalPath())
+            )
+        )
+        raise error
+
+def migrate_row(data_adapter, easyform, row):
+    cols = data_adapter.getColumnNames()
+    data = {}
+    for key, value in zip(cols, row):
+        is_valid_value(key, value, easyform)
+        data[key] = value
+    return data
 
 def migrate_saved_data(ploneformgen, easyform):
     for data_adapter in ploneformgen.objectValues("FormSaveDataAdapter"):
         actions = get_actions(easyform)
         action = actions.get(data_adapter.getId())
-        schema = get_fields(easyform)
         if ISaveData.providedBy(action):
-            cols = data_adapter.getColumnNames()
             has_failed_row = False
             for idx, row in enumerate(data_adapter.getSavedFormInput()):
-                if not should_migrate_row(data_adapter, easyform, idx, row, has_failed_row):
-                    has_failed_row = True
-                    continue
-
-                data = {}
-                for key, value in zip(cols, row):
-                    try:
-                        field = schema.get(key)
-                        value = value.decode("utf8")
-                        if IFromUnicode.providedBy(field) and value:
-                            value = field.fromUnicode(value)
-                        elif IDatetime.providedBy(field) and value:
-                            value = DateTime.DateTime(value).asdatetime()
-                        elif IDate.providedBy(field) and value:
-                            value = DateTime.DateTime(value).asdatetime().date()
-                        elif ISet.providedBy(field):
-                            value = set(literal_eval(value))
-                        elif INamedBlobFileField.providedBy(field):
-                            value = None
-                    except (
-                        ValueError,
-                        TypeError,
-                        ValidationError,
-                        SyntaxError,
-                        DateTime.interfaces.SyntaxError,
-                        DateTime.interfaces.TimeError,
-                        DateTime.interfaces.DateError
-                    ):
-                        # Exceptions above are often due to long living Forms, where users have changed their minds about
-                        # the Field formats/widgets...
-                        # Older datarows can break in these cases
-
-                        try:
-                            value_to_log = safe_unicode(value)
-                        except UnicodeError:
-                            # in case of file field, try to log the file name
-                            # which is seperated with a colon
-                            if ':' in value:
-                                value_to_log = safe_unicode(value.split(':')[0])
-                            else:
-                                # xxx we hope we are lucky that there are no broken characters in the first 50
-                                value_to_log = safe_unicode(value)[:50]
-
-                        logger.exception(
-                            u"Error for {}:'{}' in the {}/{} data adapter.".format(
-                                key,
-                                value_to_log,
-                                "/".join(easyform.getPhysicalPath()),
-                                data_adapter.getId(),
-                            )
-                        )
+                if not should_migrate_row(data_adapter, easyform, row, idx):
+                    if not has_failed_row:
                         logger.warning(
-                            "BEWARE: to keep data integrity, the data was not migrated for "
-                            "data adapter %s/%s.",
+                            "Number of columns does not match for all rows. Some data were skipped in "
+                            "data adapter %s/%s",
                             "/".join(easyform.getPhysicalPath()),
                             data_adapter.getId(),
                         )
-                        action.clearSavedFormInput()
-                        return
-                    data[key] = value
-                action.addDataRow(data)
+                    has_failed_row = True
+                    continue
+                try:
+                    data = migrate_row(data_adapter, easyform, row)
+                    action.addDataRow(data)
+                except Exception as e:
+                    logger.warning(
+                        "BEWARE: to keep data integrity, the data was not migrated for "
+                        "data adapter %s/%s.",
+                        "/".join(easyform.getPhysicalPath()),
+                        data_adapter.getId(),
+                    )
+                    action.clearSavedFormInput()
+                    return
