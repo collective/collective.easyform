2aa70b7cec8acae78cc22e4a954a0fc71d0a0de7
Thibaut Born <thibaut.born@kuleuven.be>
2022-10-03 14:11:31 +0200

    refactor to enable customization of saveddata migration

--- collective/easyform/migration/data.py
+++ collective/easyform/migration/data.py
@@ -11,24 +11,15 @@ from zope.schema.interfaces import IDatetime
 from zope.schema.interfaces import IFromUnicode
 from zope.schema.interfaces import ISet
 from zope.schema.interfaces import ValidationError
-
 import logging
-
 
 logger = logging.getLogger("collective.easyform.migration")
 
-def should_migrate_row(data_adapter, easyform, row, idx):
-    cols = data_adapter.getColumnNames()
-    if len(row) == len(cols):
-        return True
-    else:
-        logger.info(
-            "Column count mismatch at row %s",
-            idx,
-        )
-        return False
+class ConversionError(ValueError):
+    pass
 
-def is_valid_value(key, value, easyform):
+
+def convert_value(key, value, easyform):
     schema = get_fields(easyform)
     try:
         field = schema.get(key)
@@ -43,7 +34,7 @@ def is_valid_value(key, value, easyform):
             value = set(literal_eval(value))
         elif INamedBlobFileField.providedBy(field):
             value = None
-        return True
+        return value
     except (
         ValueError,
         TypeError,
@@ -52,7 +43,7 @@ def is_valid_value(key, value, easyform):
         DateTime.interfaces.SyntaxError,
         DateTime.interfaces.TimeError,
         DateTime.interfaces.DateError
-    ) as error:
+    ):
         # Exceptions above are often due to long living Forms, where users have changed their minds about
         # the Field formats/widgets...
         # Older datarows can break in these cases
@@ -75,42 +66,57 @@ def is_valid_value(key, value, easyform):
                 "/".join(easyform.getPhysicalPath())
             )
         )
-        raise error
+        raise ConversionError()
 
-def migrate_row(data_adapter, easyform, row):
-    cols = data_adapter.getColumnNames()
-    data = {}
-    for key, value in zip(cols, row):
-        is_valid_value(key, value, easyform)
-        data[key] = value
-    return data
 
-def migrate_saved_data(ploneformgen, easyform):
-    for data_adapter in ploneformgen.objectValues("FormSaveDataAdapter"):
-        actions = get_actions(easyform)
-        action = actions.get(data_adapter.getId())
-        if ISaveData.providedBy(action):
-            has_failed_row = False
-            for idx, row in enumerate(data_adapter.getSavedFormInput()):
-                if not should_migrate_row(data_adapter, easyform, row, idx):
-                    if not has_failed_row:
+class SavedDataMigrator(object):
+
+    def should_migrate_row(self, data_adapter, easyform, row, idx):
+        cols = data_adapter.getColumnNames()
+        if len(row) == len(cols):
+            return True
+        else:
+            logger.info(
+                "Column count mismatch at row %s",
+                idx,
+            )
+            return False
+
+    def migrate_row(self, data_adapter, easyform, row):
+        cols = data_adapter.getColumnNames()
+        data = {}
+        for key, value in zip(cols, row):
+            value = convert_value(key, value, easyform)
+            data[key] = value
+        return data
+
+    def migrate(self, ploneformgen, easyform):
+        for data_adapter in ploneformgen.objectValues("FormSaveDataAdapter"):
+            actions = get_actions(easyform)
+            action = actions.get(data_adapter.getId())
+            if ISaveData.providedBy(action):
+                has_failed_row = False
+                for idx, row in enumerate(data_adapter.getSavedFormInput()):
+                    if not self.should_migrate_row(data_adapter, easyform, row, idx):
+                        if not has_failed_row:
+                            logger.warning(
+                                "Some data were skipped in "
+                                "data adapter %s/%s.\n Use loglevel INFO for more details.",
+                                "/".join(easyform.getPhysicalPath()),
+                                data_adapter.getId(),
+                            )
+                        has_failed_row = True
+                        continue
+                    try:
+                        data = self.migrate_row(data_adapter, easyform, row)
+                        action.addDataRow(data)
+                    except ConversionError:
                         logger.warning(
-                            "Number of columns does not match for all rows. Some data were skipped in "
-                            "data adapter %s/%s",
+                            "BEWARE: to keep data integrity, the data was not migrated for "
+                            "data adapter %s/%s.",
                             "/".join(easyform.getPhysicalPath()),
                             data_adapter.getId(),
                         )
-                    has_failed_row = True
-                    continue
-                try:
-                    data = migrate_row(data_adapter, easyform, row)
-                    action.addDataRow(data)
-                except Exception as e:
-                    logger.warning(
-                        "BEWARE: to keep data integrity, the data was not migrated for "
-                        "data adapter %s/%s.",
-                        "/".join(easyform.getPhysicalPath()),
-                        data_adapter.getId(),
-                    )
-                    action.clearSavedFormInput()
-                    return
+                        action.clearSavedFormInput()
+                        return
+
