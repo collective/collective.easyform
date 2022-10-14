ec9a25d1989bd008114f81f2ce2a0cf0bea137ba
petschki <peter.mathis@kombinat.at>
2022-10-14 08:25:27 +0200

    fix dependencies

--- setup.py
+++ setup.py
@@ -46,6 +46,7 @@ setup(
         "plone.schema",
         "plone.schemaeditor>=4.0.0b1",
         "plone.supermodel",
+        "plone.restapi",
         "Products.CMFPlone>=6.0.0b1",
         "Products.validation",
         "setuptools",
