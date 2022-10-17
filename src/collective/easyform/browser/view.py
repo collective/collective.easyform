67bdb96a6451e72dffe14525ffecb4ca137e1dda
Chiruzzi Marco <chiruzzi.marco@gmail.com>
2022-10-17 16:14:55 +0200

    Implement onDisplayOverride TALES field

--- src/collective/easyform/browser/view.py
+++ src/collective/easyform/browser/view.py
@@ -398,6 +398,18 @@ class EasyFormForm(AutoExtensibleForm, form.Form):
         return header_to_inject
 
 
+    def getOnDisplayOverride(self):
+        """Evaluate form setup script TALES expression stored in the
+        onDisplayOverride field if defined
+        """
+        if self.context.onDisplayOverride:
+            get_expression(self.context, self.context.onDisplayOverride)
+
+    def render(self):
+        self.getOnDisplayOverride()
+        self.updateWidgets()
+        return super(EasyFormForm, self).render()
+
 
 class EasyFormFormWrapper(FormWrapper):
     form = EasyFormForm
