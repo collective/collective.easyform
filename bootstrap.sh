c33922c969459d642e61147738af276b64614782
Maurits van Rees <maurits@vanrees.org>
2023-01-03 09:31:25 +0100

    bootstrap.sh: use Python 3.
    Actually: removed `bootstrap.sh` and renamed `bootstrap-py3.sh` to `bootstrap.sh`.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # see https://community.plone.org/t/not-using-bootstrap-py-as-default/620
-rm -r ./lib ./include ./local ./bin
-virtualenv --clear .
-./bin/pip install -r requirements.txt
-./bin/buildout
+# rm -r ./lib ./include ./local ./bin
+python3 -m venv venv --clear
+./venv/bin/pip install -r requirements.txt
+./venv/bin/buildout bootstrap
