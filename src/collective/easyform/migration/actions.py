fad14b699e40916180bca48431b8fc2f55556019
Johannes Raggam <thetetet@gmail.com>
2025-01-16 13:10:10 +0100

    Update src/collective/easyform/migration/actions.py
    Co-authored-by: Alessandro Pisa <alessandro.pisa@gmail.com>

--- src/collective/easyform/migration/actions.py
+++ src/collective/easyform/migration/actions.py
@@ -114,7 +114,6 @@ def actions_model(ploneformgen):
         # remove outdated/deleted fields from the showfields list for saveddata adapters
         # otherwise a keyerror will be thrown when viewing the data in easyform
         if type_.name == 'collective.easyform.actions.SaveData':
-            filteredFields = []
             available_fields = [field.getName() for field in ploneformgen.fgFields()]
             filteredFields = [fieldname for fieldname in properties.get('showFields') if fieldname in available_fields]
             properties['showFields'] = filteredFields
